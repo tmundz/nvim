fb769b7027f6f8a3d3c527c4a9356e56a55b1ce8
Travis Mundy <mundyt4@mymacewan.ca>
2023-03-03 10:36:02 -0700

    Add/change cmp binds

--- lua/plugins/cmp.lua
+++ lua/plugins/cmp.lua
@@ -18,13 +18,17 @@ return{
         expand = function(args)
           require("luasnip").lsp_expand(args.body)
         end,
-      },
+      }, 
       mapping = cmp.mapping.preset.insert({
         ["<C-b>"] = cmp.mapping.scroll_docs(-4),
         ["<C-f>"] = cmp.mapping.scroll_docs(4),
-        ["<C-Space>"] = cmp.mapping.complete(),
+        ["<C-j>"] = cmp.mapping.select_next_item(),
+        ["<C-k>"] = cmp.mapping.select_prev_item(),
+        
+        --["<C-Space>"] = cmp.mapping.complete(),
         ["<C-e>"] = cmp.mapping.abort(),
-        ["<CR>"] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
+        --["<CR>"] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
+        ["<Tab>"] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
       }),
       sources = cmp.config.sources({
         { name = "nvim_lsp" },
@@ -38,5 +42,6 @@ return{
         },
       },
     }
+    
   end,
 }
